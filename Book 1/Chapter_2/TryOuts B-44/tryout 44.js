889ed1f7a61340d3288c1050ce7439549bb0a803
herbsattwo45 <herbert.adogo@etihuku.com>
2026-01-21 14:41:58 +0200

    Add files via upload

---
+++ Book 1/Chapter_2/TryOuts B-44/tryout 44.js
@@ -0,0 +1,20 @@
+/* Calculate total balloons needed:
+  1. Add original guests (15) and new guests (9) first.
+  2. Multiply the total group size by 2 balloons per person.
+*/
+(15 + 9) * 2; // Returns 48
+
+
+
+
+/* Calculating total balloons using BODMAS rules:
+  
+  B - Brackets ( )   <-- Priority 1: (15 + 9) happens first
+  O - Orders **
+  D - Division /
+  M - Multiplication * <-- Priority 2: Result * 2 happens second
+  A - Addition +
+  S - Subtraction -
+*/
+
+(15 + 9) * 2; // Returns 48
