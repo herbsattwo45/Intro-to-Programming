5b54eeecfec19ac899b823ba91866b90b9fb5c35
herbsattwo45 <herbert.adogo@etihuku.com>
2026-01-21 15:21:35 +0200

    Add files via upload

---
+++ Book 1/Chapter_2/TryOutsB-50/tryout50.js
@@ -0,0 +1,23 @@
+// Start with 100 balloons
+var balloons = 100;
+
+// *= means "multiply and put the answer back into the variable"
+// This is the same as: balloons = balloons * 2
+balloons *= 2;
+
+// 100 × 2 = 200
+// Result:
+balloons; // 200
+
+
+
+// Start again with 100 balloons
+var balloons = 100;
+
+// /= means "divide and put the answer back into the variable"
+// This is the same as: balloons = balloons / 4
+balloons /= 4;
+
+// 100 ÷ 4 = 25
+// Result:
+balloons; // 25
