deaddae05a41f22b9428101832da398303644526
herbsattwo45 <herbert.adogo@etihuku.com>
2026-02-02 13:48:03 +0200

    Add files via upload

---
+++ Book 2/Chapter 2/escape.js
@@ -0,0 +1,9 @@
+
+let text = "We are the so-called \"Vikings\" from the north.";
+console.log(text);
+
+let text2 = 'It\'s alright.'
+console.log(text2);
+
+let text3 = 'It\'s cool.'
+console.log(text3,);
